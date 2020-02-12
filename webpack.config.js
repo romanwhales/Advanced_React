bb86b0d5b140cdf8fefe577b8fa2afb01364bfbb
Olawale Sunmonu-Adedeji <olawalesunmonu-adedeji@Olawales-MacBook-Pro.local>
2020-02-12 23:16:43 +0100

    Fullstack Javascript with Reactjs

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,16 @@
 const path = require('path');
 
-
 const config = {
-    entry: ['babel-polyfill','./lib/components/Index.js'],
-    output:{
-        path: path.resolve(__dirname,'public'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        modules: [
-          path.join(__dirname, "src"),
-          "node_modules"
-        ]
-       },
-    module:{
-        rules: [
-            {test: /\.js$/,exclude: /node_modules/,use: 'babel-loader'}
-        ]
-    }
-}
+  entry: ['babel-polyfill', './lib/components/Index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
+    ]
+  }
+};
 
-module.exports = config
+module.exports = config;
